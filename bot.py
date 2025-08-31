a1f01a6c4aba99c9c472ad4849da1fb8c489a272
R3ZNOV <109448218+R3ZNOV@users.noreply.github.com>
2025-08-31 09:03:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 import yt_dlp
 
-TOKEN = 'MTQxMTU2NDI5ODM2MjQyMTMwOQ.GmLEml._3iLcnFYxxPX0OMM2dYmEtk8mDHmdnYFhUy8Jo'
+
 
 intents = discord.Intents.default()
 intents.message_content = True
